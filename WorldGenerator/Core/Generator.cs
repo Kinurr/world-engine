494637c691aae3310750ebe0c9f468446a360d8e
Kinurr <josefigueiredo2001@hotmail.com>
2022-05-09 21:25:13 +0100

    Added project folder

---
+++ WorldGenerator/Core/Generator.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace WorldGenerator.Core;
+
+public class Generator
+{
+    private static int _randomValue;
+
+    private static readonly Random Rng = new();
+
+    private static Bitmap? _noiseMap;
+    
+    /// <summary>
+    /// Returns simple noise in the form of a monochrome bitmap
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    public static Bitmap? GenerateSimpleNoise(int width, int height)
+    {
+        _noiseMap = new Bitmap(width, height);
+
+        for (var x = 0; x < _noiseMap.Width; x++)
+        {
+            for (var y = 0; y < _noiseMap.Height; y++)
+            {
+                _randomValue = Rng.Next(0, 255);
+                _noiseMap.SetPixel(x, y, Color.FromArgb(_randomValue, _randomValue, _randomValue));
+            }
+        }
+
+        return _noiseMap;
+    }
+}
---
+++ WorldGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using WorldGenerator.Core;
+using WorldGenerator.Utils;
+
+Console.WriteLine("Generating map...");
+
+Bitmap? noiseMap = Generator.GenerateSimpleNoise(512, 512);
+
+IOUtils.SaveBitmapLocally("noisemap", noiseMap);
+
+Console.WriteLine("Map generated and saved.");
