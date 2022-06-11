14fea38d90222a94b87132ce73174b68d23d1441
Kinurr <josemfigueiredo@outlook.pt>
2022-06-11 22:34:38 +0100

    Restructured as library

---
+++ src/MapMaker/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WorldGenerator;
+
+Console.WriteLine("Hello, World!");
+Generator.TestGenerate();
---
+++ src/WorldGenerator/Generator.cs
@@ -0,0 +1,25 @@
+﻿using SkiaSharp;
+using WorldGenerator.Core;
+using WorldGenerator.Rules;
+using WorldGenerator.Utils;
+
+namespace WorldGenerator;
+
+public static class Generator
+{
+    public static void TestGenerate()
+    {
+        Console.WriteLine("Printed from library.");
+        
+        Console.WriteLine("Generating map...");
+
+        var rules = new Ruleset("Da Rules");
+
+        SKBitmap landMap = NoiseGenerator.GenerateNoise(1920, 1080, FastNoise.NoiseType.PerlinFractal, 0.0025f, 12);
+        SKBitmap heightMap = NoiseGenerator.GenerateNoise(1920, 1080, FastNoise.NoiseType.PerlinFractal, 0.0025f, 12);
+
+        IOUtils.SaveSKBitmapLocally(Directory.GetCurrentDirectory() + "/map.png", TerrainPainter.PaintTerrain(landMap, heightMap));
+
+        Console.WriteLine("Map generated and saved.");
+    }
+}
