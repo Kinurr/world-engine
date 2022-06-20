8f996fe58a86c900e42c3ab148595aaf157cd6fc
Kinurr <josefigueiredo2001@hotmail.com>
2022-06-20 23:48:36 +0100

    logs

--- src/MapMaker/Program.cs
+++ src/MapMaker/Program.cs
@@ -1,21 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Write("Hello world!");
-
-// using WorldGenerator;
-// using WorldGenerator.Layers;
-// using WorldGenerator.Rules;
+using System.Diagnostics;
+using WorldGenerator;
+using WorldGenerator.Layers;
+using WorldGenerator.Rules;
 
 // Library usage.
 Console.WriteLine("Test generating world...");
 
-// var ruleset = new Ruleset("Da Rules", (int)DateTime.Now.Ticks);
-//
-// var layers = new List<Layer>();
-//
-// layers.Add(new Layer("Water", ruleset, LayerTypes.Water));
-// layers.Add(new Layer("Altitude", ruleset, LayerTypes.Altitude));
-//
-// var generator = new Generator(ruleset, layers);
-//
-// var world = generator.GenerateWorld();
+var ruleset = new Ruleset("Da Rules", (int)DateTime.Now.Ticks);
+
+var layers = new List<Layer>();
+
+layers.Add(new Layer("Water", ruleset, LayerTypes.Water));
+layers.Add(new Layer("Altitude", ruleset, LayerTypes.Altitude));
+
+var generator = new Generator(ruleset, layers);
+
+var world = generator.GenerateWorld();
+
+Stopwatch stopwatch = new Stopwatch();
+ 
+stopwatch.Start();
+
+WorldGenerator.Debug.Tools.SaveWorldAsPng(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "map.png", world);
+
+stopwatch.Stop();
+
+TimeSpan ts = stopwatch.Elapsed;
+string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts.Hours, ts.Minutes, ts.Seconds,
+    ts.Milliseconds / 10);
+
+ 
+Console.WriteLine("Elapsed Time is {0}", elapsedTime);
